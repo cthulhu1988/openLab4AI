12c4e52ee08c9984bbb17d4c7ce6a5b3f2021d67
isaac.callisonSD <isaac.callisonsd@mtsu.edu>
2019-11-17 19:31:03 -0600

    switching to new branch to use pandas

--- kClustering.py
+++ kClustering.py
@@ -37,6 +37,12 @@ def main():
 
 
     # The program should perform a K-means clustering by first initializing K centroid vectors (no class labels included) using K random examples from the training data
+    initializing_kNearest = KNearest(num_clusters)
+    initializing_kNearest.fit(train_list, train_label)
+    predictions = initializing_kNearest.predictClass(test_list)
+
+    acc = np.sum(predictions == test_label) / len(predictions)
+    print(acc)
 
     # The program should then determine the closest vector to each training example (using Euclidean distance), and create a new set of vectors by averaging the feature vectors of the closest training examples.
     # The program should repeat the previous step until the centroid vectors no longer change (i.e. until all training examples are assigned to the same vector on two consequtive iterations)
@@ -44,13 +50,29 @@ def main():
     # Finally, the program will calculate the closest vector to each testing example and determine if the cluster label and the testing example label match (a correct classification).
     # The program should then output the number of testing examples classified correctly by the K-means clustering
 class KNearest:
-    def __init__(self):
-        pass
-    def fit(self):
-        pass
+    def __init__(self, k=3):
+        self.k = k
 
-    def predictClass(self):
-        pass 
+    def fit(self, X, y):
+        self.X_train = X
+        self.yTrain = y
+
+
+    def predictClass(self,X):
+        # fro each sample we call helper pethod
+        predicted_labels = [self._predict(x) for x in X]
+        return np.array(predicted_labels)
+
+    def _predict(self, x):
+        distance = [distanceBetweenPoints(x, x_train) for x_train in self.X_train]
+        # compute distances
+        k_indices = np.argsort(distance)[:self.k]
+        k_nearLabels =  [self.yTrain[i] for i in k_indices]
+        # get k KNearest samples and labels
+
+        # majority vote for most common class label
+        most_common = Counter(k_nearLabels).most_common(1)
+        return most_common[0][0]
 
 
 
