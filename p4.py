21baf70f50e717b130a0c3a169b8d9802bf3be51
loki <loki@pop-os.localdomain>
2019-11-18 13:33:14 -0600

    just need to test data coming in

--- p4.py
+++ p4.py
@@ -16,16 +16,21 @@ def pick_centroids(data, num_clusters):
             number_list.append(val)
         number_list = np.array(number_list)
         new_center = Centroid(number_list)
-        new_center.name = x
         centroid_list.append(new_center)
     return centroid_list
 
-def first_pass(centroids, data):
+def pass_through(centroids, data):
+    for cent in centroids:
+        cent.pointList = []
+        cent.class_labels = []
     # iterate through rows
     for y in range(len(data.index)):
-        num_list = []
-        # strip off class label
-        info = (data.iloc[y,:-1])
+        num_list = [] ; class_label_list= []
+        # strip off class label, store it in class_labels
+        info = (data.iloc[y,:-1]) ; class_label = data.iloc[y,-1:]
+
+        for l in class_label:
+            class_label_list.append(int(l))
         # append each val to a list and covert to np array 1d
         for v in info:
             num_list.append(v)
@@ -40,61 +45,54 @@ def first_pass(centroids, data):
                 min_distance = delta
                 idx = i
                 lne = numLn
+                cls = class_label_list
         centroids[idx].pointList.append(lne)
-        idx, lne = 0,0 ; min_distance = 1000000
+        centroids[idx].class_labels.append(cls)
+        idx, lne, cls = 0,0, -1 ; min_distance = 1000000
 
 def distanceBetweenPoints(x1, x2):
-    #return np.sqrt(np.sum( (x1 - x2)**2 ))
+    # nifty numpy function found in the literatrure
     return np.linalg.norm(x2 - x1)
 
+def centroids_all_true(centroid_vector):
+    is_done = True
+    for counter, center in enumerate(centroid_vector):
+        if (center.done == False):
+            is_done = False
+    return is_done
+
 def main():
+    #################SET UP DATA #############################
     random_seed = int(sys.argv[1])
     num_clusters = int(sys.argv[2])
     training_data = sys.argv[3] # string
     testing_data = sys.argv[4] # string
 
-    # The program should read in the training data file (one training example per line, see below)
     training_data_df = pd.read_csv(training_data, sep=" ", header=None)
     num_train_rows = len(training_data_df.index)
     num_train_attributes = len(training_data_df.columns)-1
-
-    # The program should read in the testing data file (one testing example per line, see below)
     testing_data_df = pd.read_csv(testing_data, sep=" ", header=None)
 
-
-    # The program should perform a K-means clustering by first initializing K centroid vectors (no class labels included) using K random examples from the training data
+    ################# PICK CENTROIDS ######################################################
     centroid_vector = pick_centroids(training_data_df, num_clusters)
 
-    # first pass sets up the inital clustering.
-    first_pass(centroid_vector, training_data_df)
-    for i in centroid_vector:
-        print("BEFORE PASS ",i.loc)
-
-    for j in centroid_vector:
-        print("After Pass ")
-        j.update_position(num_train_attributes)
-
-    for x in centroid_vector:
-        print("AFTER PASS ",x.loc)
-
+    #################### LOOP THROUGH AND ADJUST CENTROIDS #######################################
     done = False
     max_iter = 0
     while not done:
         max_iter +=1
-        first_pass(centroid_vector, training_data_df)
-        print()
-        for i in centroid_vector:
-            print("BEFORE PASS ",i.loc)
-        for ii in centroid_vector:
-            j.update_position(num_train_attributes)
-        if centroid_vector[0].done == True or max_iter > 30:
+        if centroids_all_true(centroid_vector) or max_iter > 300:
             done = True
+        else:
+            pass_through(centroid_vector, training_data_df)
+            for ii in centroid_vector:
+                ii.update_position(num_train_attributes)
+    ########### MAJORITY VOTE CLASS LABEL #####################
+    for i in centroid_vector:
+        i.name = i.majority_cls()
 
 
 
-    # The program should then determine the closest vector to each training example (using Euclidean distance), and create a new set of vectors by averaging the feature vectors of the closest training examples.
-    # The program should repeat the previous step until the centroid vectors no longer change (i.e. until all training examples are assigned to the same vector on two consequtive iterations)
-    # Once the mean cluster vectors have been calculated, a class label will be assigned to each vector by taking a majority vote amongst it's assigned examples from the training set (ties will be broken by preferring the smallest integer class label).
     # Finally, the program will calculate the closest vector to each testing example and determine if the cluster label and the testing example label match (a correct classification).
     # The program should then output the number of testing examples classified correctly by the K-means clustering
 
@@ -105,26 +103,46 @@ class Centroid:
         self.name = ""
         self.done = False
         self.numCols = 0
+        self.class_labels = []
 
     def update_position(self, num):
+        # number of attributes
         self.numCols = num
+        # make sure pointList is not empty, temp save old location
+        temp_old = self.loc
         if len(self.pointList) > 0:
+            # turn all points into a DataFrame to get mean
             df = pd.DataFrame(self.pointList)
-            for j in range(num):
-                new_loc = (df[j].mean())
-                self.check_done(self.loc, new_loc)
-                self.loc[j] = new_loc
+            # for range of attributes
+            new_loc_list = []
+            for x in range(self.numCols):
+                new_loc_list.append(df[x].mean())
+            new_loc_np = np.array(new_loc_list)
+            self.check_done(self.loc, new_loc_np)
+            self.loc = new_loc_np
 
-    def check_done(self, loc, new_loc):
-            sum_total = np.linalg.norm(loc-new_loc)
+    def check_done(self, old_loc, new_loc):
+            sum_total = np.linalg.norm(old_loc-new_loc)
+            #print("sum total {}".format(sum_total))
             if sum_total < 0.01:
                 self.done = True
 
     def print_points(self):
-        print("NAME ",self.name)
+        print("NAME  {} and LENG {}".format(self.name, len(self.pointList)))
         for i in self.pointList:
             print("Line {}".format(i))
 
+    def majority_cls(self):
+        max = 0
+        maj = -1
+        a = np.array(self.class_labels)
+        unique, counts = np.unique(a, return_counts=True)
+        values = dict(zip(unique, counts))
+        for k,v in values.items():
+            if v > max:
+                max = v
+                maj = k
+        return maj
 
 
 if __name__ == '__main__':
